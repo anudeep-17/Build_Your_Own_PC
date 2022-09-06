105110f6fbae093301abfed15ed17bac7299ba9c
anudeep-17 <anudeepsai88@gmail.com>
2022-09-06 11:39:38 -0400

    starting assignment3

---
+++ src/assignment3/Mainrun.java
@@ -0,0 +1,20 @@
+package assignment3;
+
+public class Mainrun 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("--------------------------------------bit test -----------------------------------");
+		bit_test.runTests();
+		
+		System.out.println();
+		System.out.println("--------------------------------------Longword test -----------------------------------");
+		Longword_test.runtests();
+		
+		System.out.println();
+		
+		
+	}
+
+}
